c5336ee0937a63a8e22c5d4407db95a8ea66cdc6
Leon Machens <leon@machens.koeln>
2021-05-10 16:49:27 +0200

    ➕ Add Typescript support (#1)
    * :heavy_plus_sign: Add TypeScript
    
    * :recycle: Convert js in ts
    
    * :heavy_plus_sign: Add eslint typescript support
    
    * :label: Add tsconfig
    
    * :label: Add Node types
    
    * :art: Add server
    
    * :see_no_evil: Add stylelint ignore list

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,10 @@
 import './style.css';
 
-document.querySelector('#app').innerHTML = `
+const app = document.querySelector('#app');
+
+if (app !== null) {
+  app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
+}
