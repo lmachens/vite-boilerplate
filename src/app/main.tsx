c2a760a4d6d3d1688f2a4f2c82df6e7c9866d473
Philipp <philipp@gartz.dev>
2021-05-17 14:30:29 +0200

    ✨ Integrate react (#2)
    * ➕ Install react and react-dom with types
    
    * 🔧 Fast refresh for react in vite
    
    * 🔧 Update tsconfig for react
    
    * 💄 Rename style file
    
    * 🎨 Move favcion to app folder an rename id to match react
    
    * 🔧 Remove svg from lint-staged
    
    * 🔧 Update package-lock.json
    
    * ♻️ Refactor main.tsx to use react
    
    * ✨ Create react starter
    
    * 🔨 Move tsc to test script
    
    * 🏷️ Add explicit type
    
    * ♻️ Use querySelector

---
+++ src/app/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './globals.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.querySelector('#root')
+);
