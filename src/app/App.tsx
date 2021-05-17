9aff9c4008fa2baf06abf599d282be632523c456
phlgr <philipp@gartz.dev>
2021-05-17 12:21:56 +0200

    🏷️ Add explicit type

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 
 function App(): JSX.Element {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
 
   return (
     <div className={styles.App}>
