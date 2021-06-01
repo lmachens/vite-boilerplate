ce74f91b6670c7c1a642b652ab1a4cb657395fcd
lmachens <leon@machens.koeln>
2021-06-01 10:54:05 +0200

    ✨ Add MemoryRouter to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import { addDecorator } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,3 +10,5 @@ export const parameters = {
     },
   },
 };
+
+addDecorator((story) => <MemoryRouter>{story()}</MemoryRouter>);
