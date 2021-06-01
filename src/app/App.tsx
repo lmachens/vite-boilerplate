da918333c86825a48b5b199097d2482de305edfb
Leon Machens <leon@machens.koeln>
2021-06-01 11:07:14 +0200

    ✨ Add React-Router-Dom (#5)
    * ➕ Add React-Router-Dom
    
    https://www.npmjs.com/package/react-router-dom
    
    * ✨ Add Router with sample routes
    
    * ✨ Add MemoryRouter to storybook

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,44 +1,55 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import styles from './App.module.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
   const [count, setCount] = useState<number>(0);
 
   return (
-    <div className={styles.App}>
-      <header className={styles['App-header']}>
-        <img src={logo} className={styles['App-logo']} alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className={styles['App-link']}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className={styles['App-link']}
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div className={styles.App}>
+        <header className={styles['App-header']}>
+          <img src={logo} className={styles['App-logo']} alt="logo" />
+          <p>Hello Vite + React!</p>
+          <p>
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is: {count}
+            </button>
+          </p>
+          <p>
+            Edit <code>App.tsx</code> and save to test HMR updates.
+          </p>
+          <p>
+            <a
+              className={styles['App-link']}
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            {' | '}
+            <a
+              className={styles['App-link']}
+              href="https://vitejs.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Vite Docs
+            </a>
+          </p>
+          <Switch>
+            <Route path="/about">
+              <main>About</main>
+            </Route>
+            <Route path="/">
+              <main>Home</main>
+            </Route>
+          </Switch>
+        </header>
+      </div>
+    </Router>
   );
 }
 
