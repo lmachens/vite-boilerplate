43466b85be464d7ab07d11481ed28c413d56bfaf
lmachens <leon@machens.koeln>
2021-06-07 10:13:21 +0200

    ✨ Add simple Express server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
-import fs from 'fs';
+import express from 'express';
 
-console.log(fs.readFileSync('./README.md', 'utf-8'));
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`photoplay app listening at http://localhost:${PORT}`);
+});
