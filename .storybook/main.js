1f4c09dbf966124c5eef8b1cd29f4071692ba880
lmachens <leon@machens.koeln>
2021-07-04 12:43:56 +0200

    ⬆️ Upgrade Storybook to support Vite This has a huge performance impact. It is required to update Storybook and Vite to the latest version. `storybook-css-modules-preset` is not required anymore because of Vite support for CSS modules. More details: https://storybook.js.org/blog/storybook-for-vite/

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,12 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    'storybook-css-modules-preset',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: 'storybook-builder-vite',
+  },
+  async viteFinal(config) {
+    // Set relative base path to support deployment on path like /storybook
+    config.base = './';
+    return config;
+  },
 };
