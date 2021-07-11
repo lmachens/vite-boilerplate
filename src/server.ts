44dd12a86dcf6457331b2c24ae4d1f9083610754
lmachens <leon@machens.koeln>
2021-07-11 12:52:54 +0200

    🐛 Handle client routing

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import router from './lib/router';
+import path from 'path';
 
 const { PORT = 3001 } = process.env;
 
@@ -16,6 +17,11 @@ app.use('/storybook', express.static('dist/storybook'));
 
 // Serve app production bundle
 app.use(express.static('dist/app'));
+
+// Handle client routing, return all requests to the app
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'app/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
