322a74d0711984490394fda34d296ec30e3a157c
Ronan-Yann Lorin <ryl@free.fr>
2023-05-19 19:04:53 +0200

    production build fix

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,25 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@chakra-ui/storybook-addon'],
+  // addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@chakra-ui/storybook-addon'], // Current chakra addon is incompatible
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
+  },
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
-  // async viteFinal(config) {
-  //   // Set relative base path to support deployment on path like /storybook
-  //   config.base = './';
-  //   config.optimizeDeps.include = ['react-is', '@base2/pretty-print-object', ...config?.optimizeDeps?.include];
-  //   return config;
-  // },
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      // Set relative base path to support deployment on path like /storybook
+      base: './',
+      optimizeDeps: { include: ['react-is', '@base2/pretty-print-object'] },
+    });
+  },
   docs: {
     autodocs: true,
   },
--- src/app/components/Layout/Layout.stories.tsx
+++ src/app/components/Layout/Layout.stories.tsx
@@ -1,10 +1,19 @@
-import React, { ReactNode } from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
 
 import Layout from './Layout';
 
-export default {
+const meta = {
   title: 'Component/Layout',
   component: Layout,
-};
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+} satisfies Meta<typeof Layout>;
 
-export const Default = (props: { children: ReactNode }): JSX.Element => <Layout>{props.children}</Layout>;
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Sample: Story = {
+  args: {
+    children: 'Sample layout',
+  },
+};
